98d7bc78fa1a07298a5f26365328e3f3473e2749
Elena <rhapsodyenmauve@gmail.com>
2021-08-28 03:56:57 +0100

    stack overflow example in code

--- examples.js
+++ examples.js
@@ -36,3 +36,33 @@ setTimeout(() => {
   console.log('2'), 1000;
 });
 console.log('3');
+
+// ******************************************************************
+// this is causing STACK OVERFLOW!!!
+//fill array with 60000 elements
+const list = new Array(60000).join('1.1').split('.');
+
+function removeItemsFromList() {
+  var item = list.pop();
+
+  if (item) {
+    removeItemsFromList();
+  }
+}
+
+removeItemsFromList();
+
+// SOLUTION
+// and this ⬇️ fixes the problem
+const list = new Array(60000).join('1.1').split('.');
+
+function removeItemsFromList() {
+  var item = list.pop();
+
+  if (item) {
+    setTimeout(removeItemsFromList, 0);
+  }
+}
+
+removeItemsFromList();
+// ******************************************************************
